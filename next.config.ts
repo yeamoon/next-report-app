d6804c0b72d67715b908a34b6d9dc825035dfd0c
yeamoon <yeamoon@users.noreply.github.com>
2024-12-28 17:16:05 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: 'export', // Ensures the app is exported as static files
   basePath: '/next-report-app', // Use the repository name here
-  assetPrefix: '/next-report-app/', // Prefix static assets with the repository name
+  
 };
 
 module.exports = nextConfig;
