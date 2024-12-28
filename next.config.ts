3413590c4832f902b90d1731223df00bcc04422f
yeamoon <yeamoon@users.noreply.github.com>
2024-12-28 18:37:28 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
-module.exports = {
-  basePath: '/next-report-app',  // This should match the subpath for your GitHub Pages deployment
-  assetPrefix: '/next-report-app', // Ensures assets like JS, CSS, images are loaded from the right path
-  output: 'export', // Make sure you are using the export output format for static export
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Enables static export
+  
 };
+
+module.exports = nextConfig;
