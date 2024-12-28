929e665d9a1c4768a37bc086ca95c5286cbea37e
yeamoon <yeamoon@users.noreply.github.com>
2024-12-28 02:04:27 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,4 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = {
+  output: 'export',
+  trailingSlash: true
+}
